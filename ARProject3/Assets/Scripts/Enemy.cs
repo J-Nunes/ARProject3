c89de2ad9f0ed3a15e994ed2d8cf7a2b4d095121
Jordi Nunes <jordinunes96@gmail.com>
2017-05-08 18:26:50 +0200

    1st commit

---
+++ ARProject3/Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform attack_pos;
+    public NavMeshAgent agent;
+    public float radius;
+
+	// Use this for initialization
+	void Start ()
+    {
+        CalcRandomPos();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+	}
+
+    void CalcRandomPos()
+    {
+        Vector2 random = Random.insideUnitCircle * radius;
+        Debug.Log(random);
+        Vector3 destination;
+
+        destination = attack_pos.position;
+        destination.x += random.x;
+        destination.z += random.y;
+
+        agent.SetDestination(destination);
+    }
+}
