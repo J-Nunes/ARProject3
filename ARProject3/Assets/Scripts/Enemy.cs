14603d0715d44c09a39b86a542ae2f09cc42b9d3
AGM16 <adri_67@hotmail.es>
2017-05-09 14:02:32 +0200

    Respawn

--- ARProject3/Assets/Scripts/Enemy.cs
+++ ARProject3/Assets/Scripts/Enemy.cs
@@ -5,32 +5,68 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    public Transform attack_pos;
+   
+    Transform position_enemy;
+    public Transform position_attack;
     public NavMeshAgent agent;
+    Vector3 destination;
+
+    GameManager g_manager;
     public float radius;
 
-	// Use this for initialization
-	void Start ()
+    void Awake()
     {
+        position_enemy = GetComponent<Transform>();
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        //Choose Respawn at the beginning
+        Choose_Respawn();
+       
+        //After the respawn of the character, this goes to the attack position
         CalcRandomPos();
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        
-	}
+     
+    }
 
     void CalcRandomPos()
     {
         Vector2 random = Random.insideUnitCircle * radius;
         Debug.Log(random);
-        Vector3 destination;
+       
 
-        destination = attack_pos.position;
+        destination = position_attack.position;
         destination.x += random.x;
         destination.z += random.y;
 
         agent.SetDestination(destination);
     }
+
+    void Choose_Respawn()
+    {
+        //Choose Respawn
+        g_manager = GameObject.Find("Game_Manager").GetComponent<GameManager>();
+
+        int value_respawn = Random.Range(1, 10);
+     
+
+        if (value_respawn <= 5)
+        {
+            position_enemy.position = g_manager.Respawn_2.position;
+        }
+        else
+        {
+            position_enemy.position = g_manager.Respawn_1.position;
+        }
+
+        
+    }
+
+  
 }
---
+++ ARProject3/Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager g_manager;
+    public Transform Respawn_1;
+    public Transform Respawn_2;
+    public int max_soldiers_capacity = 10;
+    public int min_soliders_Capacity = 5;
+
+    List<GameObject> soldiers = new List<GameObject>();
+    public GameObject[] people;
+
+    //Attack Positions
+    public Transform attack_pos1_respawn1;
+    public Transform attack_po2_respawn1;
+    public Transform attack_pos1_respawn2;
+    public Transform attack_po2_respawn2;
+
+
+    void Awake()
+    {
+        g_manager = this;
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+	}
+
+   
+}
